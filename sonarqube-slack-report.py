140061eeb03f5f247066caa46a19ec213399e5d4
Raushan Raj <54439583+sttor@users.noreply.github.com>
2022-07-04 08:26:21 +0530

    Create sonarqube-slack-report.py

---
+++ sonarqube-slack-report.py
@@ -0,0 +1,69 @@
+import requests, json, os, time, sys
+
+class SonarQubeReportSlack:
+
+    def __init__(self):
+        self.slack_token = os.getenv("slack_token")
+        self.fail_build = os.getenv("fail_build", "false")
+        self.component = os.getenv("component")
+        self.slack_channel = os.getenv("slack_channel")
+        self.sonar_url = os.getenv("sonar_url")
+        self.sonar_username = os.getenv("sonar_username")
+        self.sonar_password = os.getenv("sonar_password")
+
+    def wait_for_analysis(self):
+        ATTEMPTS = 10
+        url = self.sonar_url + "/api/ce/component?component=%s"%(self.component,)
+        while True:
+            res = requests.get(url, auth=(self.sonar_username, self.sonar_password)).json()
+            print(res)
+            if "queue" not in res.keys() or not res["queue"] or ATTEMPTS == 0:
+                break
+            time.sleep(10)
+            ATTEMPTS-=1
+
+
+    def generate_summary_and_report(self):
+        cmd = """sonar-report  --sonarurl="%s" --sonarusername="%s" --sonarpassword="%s"  --sonarcomponent="%s" --allbugs="false" > sonar_report.html"""
+        cmd=cmd%(self.sonar_url,self.sonar_username,self.sonar_password, self.component)
+        os.system(cmd)
+        with open('sonar_report.html') as f: report = f.read()
+        count, summary =  self.generate_summary()
+        print(summary)
+        print("::set-output name=summary::%s."%summary)
+        self.post_file_to_slack(
+             summary,
+            'Report.html',
+             report)
+        # Block Build in case of blocker
+        if int(count) > 1 and self.fail_build == "true":
+            sys.exit(1)
+
+    def generate_summary(self):
+        url = self.sonar_url + "/api/issues/search?severities=BLOCKER,CRITICAL"
+        res = requests.get(url, auth=(self.sonar_username, self.sonar_password)).json()
+        print(res)
+        count = str(res["paging"]["total"])
+        return count,"SAST %s: %s Blocker and Critical Security Issues Identified in the Repository"%(self.component,count)
+
+
+    def post_file_to_slack(
+            self, text, file_name, file_bytes, file_type=None, title='SonarQube Vulnerability Report '
+    ):
+        return requests.post(
+            'https://slack.com/api/files.upload',
+            {
+                'token': self.slack_token,
+                'filename': file_name,
+                'channels': self.slack_channel,
+                'filetype': file_type,
+                'initial_comment': text,
+                'title': title
+            },
+            files={'file': file_bytes}).json()
+
+    def run(self):
+        self.wait_for_analysis()
+        self.generate_summary_and_report()
+
+SonarQubeReportSlack().run()
